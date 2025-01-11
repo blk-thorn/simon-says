ced149573aa3447cd512358cb63825a60f5f6c62
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-01-12 02:03:24 +0300

    feat: add new buttons with functionality and styles

--- simon-says/script.js
+++ simon-says/script.js
@@ -14,6 +14,8 @@ let pressedKeys = [];
 let currentRound = 1;
 let currentDifficulty = 'easy';
 
+let tryCount = 1;
+
 
 window.onload = init;
 
@@ -22,40 +24,70 @@ function init() {
   createElements();
   generateVirtualKeyboard();
 
-  const display = document.querySelector('.display');
 
   const easyButton = document.getElementById('easy');
   const mediumButton = document.getElementById('medium');
   const hardButton = document.getElementById('hard');
 
   const startButton = document.getElementById('start');
-  const repeatButton = document.getElementById('repeat');
+  const repeatButton  = document.getElementById('repeat sequence');
+  const newGame = document.getElementById('new game');
+  const nextButton = document.getElementById('next');
+
+  easyButton.classList.add('key--active');
 
   easyButton.addEventListener('click', () => {
     currentDifficulty = 'easy';
     generateVirtualKeyboard();
+    mediumButton.classList.remove('key--active');
+    hardButton.classList.remove('key--active');
+    easyButton.classList.add('key--active');
   });
 
   mediumButton.addEventListener('click', () => {
     currentDifficulty = 'medium';
     generateVirtualKeyboard();
+    easyButton.classList.remove('key--active');
+    hardButton.classList.remove('key--active');
+    mediumButton.classList.add('key--active');
   });
 
   hardButton.addEventListener('click', () => {
     currentDifficulty = 'hard';
     generateVirtualKeyboard();
+    easyButton.classList.remove('key--active');
+    mediumButton.classList.remove('key--active');
+    hardButton.classList.add('key--active');
   });
 
   startButton.addEventListener('click', () => {
-    console.log(currentRound);
     sequence = [];
     pressedKeys = [];
     startGame();
+    mediumButton.disabled = true;
+    hardButton.disabled = true;
+    startButton.style.display = 'none';
+    repeatButton.style.display = 'flex';
+    newGame.style.display = 'flex';
   });
 
   repeatButton.addEventListener('click', () => {
+    repeatButton.disabled = true;
     repeat();
   });
+
+  nextButton.addEventListener('click', () => {
+    nextRound();
+  });
+
+  newGame.addEventListener('click', () => {
+    sequence = [];
+    pressedKeys = [];
+    startButton.style.display = 'flex';
+    repeatButton.style.display = 'none';
+    newGame.style.display = 'none';
+  });
+
 }
 
 function generateVirtualKeyboard() {
@@ -79,19 +111,27 @@ function generateVirtualKeyboard() {
 
 function handleKeyPress(symbol) {
   console.log(`Key pressed: ${symbol}`);
-
+  const nextButton = document.getElementById('next');
   pressedKeys.push(symbol);
   display.value = pressedKeys;
 
   if (pressedKeys.length === sequence.length) {
     if (isArraysEqual(pressedKeys, sequence)) {
-      console.log("Правильная последовательность:", pressedKeys);
+      display.value = "Correct!";
       currentRound++;
       setTimeout(() => {
-        nextRound();
+        nextButton.style.display = 'flex';
+        // nextRound();
+        document.querySelector('h2').textContent = `Round: ${currentRound}`;
+        document.getElementById('repeat').disabled = false;
       },1000);
+    } else if(tryCount === 2) {
+      document.getElementById('start').style.display = 'flex';
+      display.value = "You loose!";
+      pressedKeys = [];
     } else {
-      console.log("Неправильная последовательность");
+      display.value = "Wrong sequence!";
+      tryCount++
       pressedKeys = [];
     }
   }
@@ -104,7 +144,9 @@ function isArraysEqual(arr1, arr2) {
 function startGame() {
   const length = currentRound * 2;
   const symbols = difficulty[currentDifficulty];
+
   sequence = [];
+  tryCount = 1;
 
   for(let i =  0; i < length; i++) {
     sequence.push(symbols.charAt(Math.floor(Math.random() * symbols.length)));
@@ -130,10 +172,13 @@ function startGame() {
 
 function nextRound() {
   pressedKeys = [];
+  tryCount = 1;
+
   if(currentRound < 5) {
     startGame();
   }
 }
+
 function repeat() {
   const activeKeys = document.querySelectorAll('.key');
 
@@ -155,13 +200,35 @@ function createElements() {
   const h1 = document.createElement('h1');
   h1.textContent = 'Simon Says Game';
 
+  const h2 = document.createElement('h2');
+  h2.textContent = `Round: ${currentRound}`;
+
   const ul = document.createElement('ul');
-  ul.className = 'control__list';
-  ul.id = 'controls';
+  ul.className = 'level__list';
+  ul.id = 'levels';
 
-  const buttonNames = ['Easy', 'Medium', 'Hard', 'Start', 'Repeat'];
+  const ul2 = document.createElement('ul');
+  ul2.className = 'control__list';
+  ul2.id = 'controls';
 
-  buttonNames.forEach(name => {
+  const levelButtons = ['Easy', 'Medium', 'Hard', 'Start'];
+
+  levelButtons.forEach(name => {
+    const li = document.createElement('li');
+    li.className = 'level__item';
+
+    const button = document.createElement('button');
+    button.className = 'level__button';
+    button.id = name.toLowerCase();
+    button.textContent = name;
+
+    li.appendChild(button);
+    ul.appendChild(li);
+  });
+
+  const controlButtons = [ 'Repeat sequence', 'New game', "Next"];
+
+  controlButtons.forEach((name, index) => {
     const li = document.createElement('li');
     li.className = 'control__item';
 
@@ -170,8 +237,10 @@ function createElements() {
     button.id = name.toLowerCase();
     button.textContent = name;
 
+    button.style.display = 'none';
+
     li.appendChild(button);
-    ul.appendChild(li);
+    ul2.appendChild(li);
   });
 
   const virtualKeyboard = document.createElement('div');
@@ -182,9 +251,11 @@ function createElements() {
   display.className = 'display';
 
 
-  body.appendChild(h1);
+  body.append(h1);
+  body.append(h2)
   body.append(display);
-  body.appendChild(ul);
-  body.appendChild(virtualKeyboard);
+  body.append(ul);
+  body.append(ul2);
+  body.append(virtualKeyboard);
 }
 
